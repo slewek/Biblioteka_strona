858dc29be9ea0891255053fbb0f151907185010b
slewek <slewek13@gmail.com>
2018-05-17 16:58:02 +0200

    Init

---
+++ Biblioteka/Biblioteka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Biblioteka.Startup))]
+namespace Biblioteka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
